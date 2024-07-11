fbbf6e9f6f472d7ee6f9c6f46c979e12c143b2cb
LimaSahibzada <lima.sahibzada2023@gmail.com>
2024-07-11 11:18:41 +0430

    added changes for linters error

--- script.js
+++ script.js
@@ -56,7 +56,6 @@ document.getElementById('search-button').addEventListener('click', async () => {
     document.getElementById('special-attack').innerText = data.stats.find((stat) => stat.stat.name === 'special-attack').base_stat;
     document.getElementById('special-defense').innerText = data.stats.find((stat) => stat.stat.name === 'special-defense').base_stat;
     document.getElementById('speed').innerText = data.stats.find((stat) => stat.stat.name === 'speed').base_stat;
-
   } catch (error) {
     document.getElementById('error-message').innerText = 'Pokémon not found';
     clearPokemonData();
